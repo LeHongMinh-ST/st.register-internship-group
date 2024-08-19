b3c63a3c88c525af07f4cce5303726274e100cb0
minhlh <minhlh@haposoft.com>
2024-08-19 17:22:42 +0700

    init

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.scss',
+                'resources/css/auth.scss',
                 'resources/js/app.js',
                 'resources/js/campaign/index.js',
                 'resources/js/auth/login.js',
