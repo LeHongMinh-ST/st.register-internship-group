d8dd4071cecd355feeead21fe1fcb631f2605495
Trương Tuấn Hoàng <truongtuanhung09@gmail.com>
2025-01-18 17:06:04 +0700

    update-code

--- app/Models/GroupOfficial.php
+++ app/Models/GroupOfficial.php
@@ -25,11 +25,6 @@ class GroupOfficial extends Model
         return $this->belongsTo(Teacher::class);
     }
 
-    public function company(): BelongsTo
-    {
-        return $this->belongsTo(StudentGroupOfficial::class);
-    }
-
     public function groupKey(): MorphOne
     {
         return $this->morphOne(GroupKey::class, 'groupkeyable', 'group_type', 'group_id')->orderBy('created_at', 'desc');
@@ -40,8 +35,7 @@ class GroupOfficial extends Model
         if ($search) {
             $query->whereHas('students', function ($q) use ($search) {
                 $q->where('name', 'like', '%'.$search.'%')
-                    ->orWhere('code', 'like', '%'.$search.'%')
-                    ->orWhere('supervisor', 'like', '%'.$search.'%');
+                    ->orWhere('code', 'like', '%'.$search.'%');
             })
             ->orWhereHas('teacher', function ($q) use ($search) {
                 $q->where('name', 'like', '%'.$search.'%');
