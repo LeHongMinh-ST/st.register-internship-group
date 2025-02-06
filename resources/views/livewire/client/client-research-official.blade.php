e5d617a65ad72f3a4da4ac0b6ee8e41d381ee417
Trương Tuấn Hoàng <truongtuanhung09@gmail.com>
2025-02-06 20:01:38 +0700

    plan-name-modify

--- resources/views/livewire/client/client-research-official.blade.php
+++ resources/views/livewire/client/client-research-official.blade.php
@@ -179,7 +179,7 @@
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header bg-primary">
-                    <h5 class="modal-title text-white"> {{ $campaign->name }}</h5>
+                    <h5 class="modal-title text-white"> {{ $planName }}</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                 </div>
 
