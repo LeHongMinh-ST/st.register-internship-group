3d1d301e527371c56fa57ab9de98a9de1afcfc4b
Trương Tuấn Hoàng <truongtuanhung09@gmail.com>
2025-06-09 09:47:25 +0700

    600-character-description

--- app/Livewire/Teacher/Topic/TopicCreate.php
+++ app/Livewire/Teacher/Topic/TopicCreate.php
@@ -48,7 +48,7 @@ class TopicCreate extends Component
         return [
             'title' => 'required',
             'campaign_id' => 'required',
-            'description' => 'max:1000',
+            'description' => 'max:600',
         ];
     }
 }
